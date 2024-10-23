810330935d9435603acfc9124f372554178ece51
Iharry-marques <fabio.marques.melo@gmail.com>
2024-10-22 21:42:06 -0300

    numberRandomInSquare

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -11,7 +11,22 @@ const state = {
 };
 
 
+function randomSquare(){
+    state.view.squares.forEach((square)=>{
+        square.classList.remove("enemy")
+    })
+
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList("enemy")
+
+}
+
+function addListenerHitBox(){
+    state.view.squares.forEach((square)=>{});
+}
+
 function init(){}
 
 
-
+init();
